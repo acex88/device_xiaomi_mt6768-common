8e4c444a8dbdd467ea131ba8ef7d3177098ed507
bengris32 <bengris32@protonmail.ch>
2023-10-19 12:24:23 +0700

    mt6768-common: Switch to AOSP audio service
    Signed-off-by: acex88 <abhinav.115260@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -18,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service.mediatek \
+    android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     audio.primary.default \
     audio.primary.mt6768 \
