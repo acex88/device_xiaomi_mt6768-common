94029003fd1217d7184100b1acc6e5a2aad98645
acex88 <abhinav.115260@gmail.com>
2023-12-24 21:27:24 +0530

    mt6768-common: Drop unneeded audio modules
    * There are not gonna use in android 14

--- mt6768.mk
+++ mt6768.mk
@@ -20,8 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
-    audio.primary.default \
-    audio.primary.mt6768 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio-impl
 
